d567e3b422f0fa9c9b67cb08fb22a0d54e171551
Alessandro Montanari <alemonty88@gmail.com>
2017-12-08 09:59:28 +0000

    Minor change to load our custom json file for training

--- train.py
+++ train.py
@@ -71,9 +71,14 @@ def _main_(args):
     ###############################
 
     # parse annotations of the training set
-    train_imgs, train_labels = parse_annotation(config['train']['train_annot_folder'], 
-                                                config['train']['train_image_folder'], 
-                                                config['model']['labels'])
+    #train_imgs, train_labels = parse_annotation(config['train']['train_annot_folder'], 
+    #                                            config['train']['train_image_folder'], 
+    #                                            config['model']['labels'])
+
+    # Here we load our json file that contains the path the images, the BBs and the classes
+    # TODO we should return a set with the labels we have read so we can do the checks below 
+    with open(config['train']['train_annot_folder'], 'r') as f:
+        train_imgs = json.load(f)
 
     # parse annotations of the validation set, if any, otherwise split the training set
     if os.path.exists(config['valid']['valid_annot_folder']):
@@ -81,22 +86,22 @@ def _main_(args):
                                                     config['valid']['valid_image_folder'], 
                                                     config['model']['labels'])
     else:
-        train_valid_split = int(0.8*len(train_imgs))
+        train_valid_split = int(0.9*len(train_imgs))
         np.random.shuffle(train_imgs)
 
         valid_imgs = train_imgs[train_valid_split:]
         train_imgs = train_imgs[:train_valid_split]
 
     
-    overlap_labels = set(config['model']['labels']).intersection(set(train_labels.keys()))
+    #overlap_labels = set(config['model']['labels']).intersection(set(train_labels.keys()))
 
-    print 'Seen labels:\t', train_labels
-    print 'Given labels:\t', config['model']['labels']
-    print 'Overlap labels:\t', overlap_labels    
+    #print('Seen labels:\t', train_labels)
+    #print('Given labels:\t', config['model']['labels'])
+    #print('Overlap labels:\t', overlap_labels)    
 
-    if len(overlap_labels) < len(config['model']['labels']):
-        print 'Some labels have no images! Please revise the list of labels in the config.json file!'
-        return
+    #if len(overlap_labels) < len(config['model']['labels']):
+    #    print('Some labels have no images! Please revise the list of labels in the config.json file!')
+    #    return
         
     ###############################
     #   Construct the model 
@@ -113,13 +118,13 @@ def _main_(args):
     ###############################    
 
     if os.path.exists(config['train']['pretrained_weights']):
-        print "Loading pre-trained weights in", config['train']['pretrained_weights']
+        print("Loading pre-trained weights in", config['train']['pretrained_weights'])
         yolo.load_weights(config['train']['pretrained_weights'])
 
     ###############################
     #   Start the training process 
     ###############################
-
+    print("Train on", len(train_imgs), "images and validate on", len(valid_imgs), "images")
     yolo.train(train_imgs         = train_imgs,
                valid_imgs         = valid_imgs,
                train_times        = config['train']['train_times'],
